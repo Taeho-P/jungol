a9c65f0ef0f3f92d3d001b3ae66664eb2acf1371
Class01 <Class01@DESKTOP-5F4QJGN>
2023-04-26 10:40:56 +0900

    정올 9003 문제 수정

--- jungol/src/p9002/Main.java
+++ jungol/src/p9002/Main.java
@@ -3,7 +3,7 @@ package p9002;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("두번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
+		System.out.println("두 번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
 		
 
 	}
